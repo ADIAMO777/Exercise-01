6cf1b0316be3d909ed3f3d11453cf4fe4d37f8b9
Afolabi <afolabiadeolaolajumoke@gmail.com>
2025-12-01 21:46:19 +0100

    now

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let main =document.querySelector(`.main`);
-let btn =document.querySelector(`co1`);
+let btn =document.querySelector(`#co1`);
 co1.addEventListener(`click`, function(){
     if (main.style.color==`white`)
     {
@@ -11,18 +11,18 @@ co1.addEventListener(`click`, function(){
 
  main =document.querySelector(`.main`);
  btn =document.querySelector(`co2`);
-co2.addEventListener(`click`, function(){
+btn.addEventListener(`click`, function(){
         main.style.color=` red`;
 });
 
 main =document.querySelector(`.main`);
 btn =document.querySelector(`co3`);
-co3.addEventListener(`click`, function(){
+btn.addEventListener(`click`, function(){
         main.style.color=` #05c9fd`;
 });
 
 main =document.querySelector(`.main`);
 btn =document.querySelector(`co4`);
-co4.addEventListener(`click`, function(){
+btn.addEventListener(`click`, function(){
         main.style.color=` #fe02a1`;
 });
